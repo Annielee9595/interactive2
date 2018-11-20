0558db5ba2af44d5c74393617eab3f3dec3b8cb3
이보경 <ibogyeong@ibogyeong-ui-MacBook-Pro.local>
2018-11-20 14:54:33 -0800

    asdfasfd

--- info/assets/js/script.js
+++ info/assets/js/script.js
@@ -1,33 +1,6 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        alert($(document).scrollTop() + " px");
-    });
-
-
+$(window).scroll(function() {
+	var wScroll = $(this).scrollTop();
+	$('h1.logo').css({
+		'transform' : 'translate(0px, ' + wScroll / 9 + '%)'
+	});
 });
-
-
-function myFunction() {
- var pos =$(document).scrollTop();
- if(pos>1000){
-var node = document.createElement("div");
-var para = document.getElementsById("test");
-node.appendChild(para);
-
-var element = document.getElementByClassName("one");
-element.appendChild(node);
-}
-};
-
-// $(document).ready(function(){
-  
-// var node = document.createElement("div");
-// var para = document.getElementsById("test");
-// node.appendChild(para);
-
-// var element = document.getElementByClassName("one");
-// element.appendChild(node);
-// };
-
-// });
-
