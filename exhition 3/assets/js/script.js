3179affcce710611e39b7430f978fe1d40e59c16
이보경 <ibogyeong@ibogyeong-ui-MacBook-Pro.local>
2018-11-29 09:22:49 -0800

    asdfasdf

--- exhition 3/assets/js/script.js
+++ exhition 3/assets/js/script.js
@@ -30,4 +30,10 @@ element.appendChild(node);
 // };
 
 // });
+var parallax = document.getElementById("outer");
+var speed = -1;
 
+window.onscroll = function() {
+  var yOffset = window.pageYOffset;
+  parallax.style.backgroundPosition = "0px " + (yOffset / speed) + "px";
+}
