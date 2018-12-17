e8b9e663384bc1829815ab93e8957cc3f895d780
이보경 <ibogyeong@ibogyeong-ui-MacBook-Pro.local>
2018-12-16 19:24:20 -0800

    bjk

---
+++ mouseevent/assets/js/script.js
@@ -0,0 +1,565 @@
+
+  $(document).ready(function(){
+
+    // clicking 
+   
+    var A = false;
+    var B=false;
+    var C=false;
+    var D=false;
+    var E=false;
+    var F=false;
+    var G=false;
+    var H=false;
+    var I=false;
+    var J=false;
+    var K=false;
+    var L=false;
+    var N=false;
+    var M=false;
+    var O=false;
+    var P=false;
+    var Q=false;
+    var R=false;
+    var S=false;
+    var T=false;
+    var U=false;
+    var V=false;
+    var W=false;
+    var X=false;
+    var Y=false;
+    var Z=false;
+    var a=false;
+    var b=false;
+    var c=false;
+    var d=false;
+    var e=false;
+    var f=false;
+    var g=false;
+    var h=false;
+    var i=false;
+    var j=false;
+    var k=false;
+    var l=false;
+    var m=false;
+    var n=false;
+    var o=false;
+    var p=false;
+    var q=false;
+    var r=false;
+    var s=false;
+    var t=false;
+    var u=false;
+    var v=false;
+    var w=false;
+    var x=false;
+    var y=false;
+    var z=false;
+    var emoji1=false;
+    var emoji2=false;
+    var emoji3=false;
+    var emoji4=false;
+    var emoji5=false;
+    var emoji6=false;
+    var emoji7=false;
+    var emoji8=false;
+    var emoji9=false;
+    var emoji10=false;
+    var emoji11=false;
+    var emoji12=false;
+    var emoji13=false;
+    var emoji14=false;
+  
+    // x and y pos
+    var xPos;
+    var yPos;
+
+    $('body').mouseup(function(){
+      A= false;
+      B= false;
+      C= false;
+      D=false;
+      E=false;
+      F=false;
+      G=false;
+      H=false;
+      I=false;
+      J=false;
+      K=false;
+      L=false;
+      N=false;
+      M=false;
+      O=false;
+      P=false;
+      Q=false;
+      R=false;
+      S=false;
+      T=false;
+      U=false;
+      V=false;
+      W=false;
+      X=false;
+      Y=false;
+      Z=false;
+      a=false;
+      b=false;
+      c=false;
+      d=false;
+      e=false;
+      f=false;
+      g=false;
+      h=false;
+      i=false;
+      j=false;
+      k=false;
+      l=false;
+      m=false;
+      n=false;
+      o=false;
+      p=false;
+      q=false;
+      r=false;
+      s=false;
+      t=false;
+      u=false;
+      v=false;
+      w=false;
+      x=false;
+      y=false;
+      z=false;
+      emoji1=false;
+      emoji2=false;
+      emoji3=false;
+      emoji4=false;
+      emoji5=false;
+      emoji6=false;
+      emoji7=false;
+      emoji8=false;
+      emoji9=false;
+      emoji10=false;
+      emoji11=false;
+      emoji12=false;
+      emoji13=false;
+      emoji14=false;
+      
+
+    })
+                  
+    $('#A').click(function(){
+      A= true;
+    });  
+
+     $('#B').click(function(){
+      B= true;
+    });  
+$('#C').click(function(){
+      C= true;
+});
+  $('#D').click(function(){
+      D= true;
+    }); 
+  $('#E').click(function(){
+      E= true;
+    }); 
+  $('#F').click(function(){
+      F= true;
+    }); 
+  $('#G').click(function(){
+      G= true;
+    }); 
+  $('#H').click(function(){
+      H= true;
+    }); 
+  $('#I').click(function(){
+      I= true;
+    });     
+  $('#J').click(function(){
+      J= true;
+    }); 
+  $('#K').click(function(){
+      K= true;
+    }); 
+  $('#L').click(function(){
+      L= true;
+    }); 
+  $('#N').click(function(){
+      N= true;
+    }); 
+  $('#M').click(function(){
+      M= true;
+    }); 
+  $('#O').click(function(){
+      O= true;
+    }); 
+  $('#P').click(function(){
+      P= true;
+    }); 
+  $('#Q').click(function(){
+      Q= true;
+    });     
+  $('#R').click(function(){
+      R= true;
+    }); 
+  $('#S').click(function(){
+      S= true;
+    });   
+  $('#T').click(function(){
+      T= true;
+    }); 
+  $('#U').click(function(){
+      U= true;
+    }); 
+  $('#V').click(function(){
+      V= true;
+    }); 
+  $('#W').click(function(){
+      W= true;
+    }); 
+  $('#X').click(function(){
+      X= true;
+    });     
+  $('#Y').click(function(){
+      Y= true;
+    }); 
+  $('#Z').click(function(){
+      Z= true;
+    }); 
+    $('#a').click(function(){
+      a= true;
+    }); 
+    $('#b').click(function(){
+      b= true;
+    });
+    $('#c').click(function(){
+      c= true;
+    }); 
+    $('#d').click(function(){
+      d= true;
+    }); 
+    $('#e').click(function(){
+      e= true;
+    }); 
+    $('#f').click(function(){
+      f= true;
+    }); 
+    $('#g').click(function(){
+      g= true;
+    }); 
+    $('#h').click(function(){
+      h= true;
+    });
+    $('#i').click(function(){
+      i= true;
+    }); 
+    $('#j').click(function(){
+      j= true;
+    }); 
+    $('#k').click(function(){
+      k= true;
+    }); 
+    $('#l').click(function(){
+      l= true;
+    }); 
+    $('#m').click(function(){
+      m= true;
+    }); 
+    $('#n').click(function(){
+      n= true;
+    });
+    $('#o').click(function(){
+      o= true;
+    }); 
+    $('#p').click(function(){
+      p= true;
+    }); 
+    $('#q').click(function(){
+      q= true;
+    }); 
+    $('#r').click(function(){
+      r= true;
+    }); 
+    $('#s').click(function(){
+      s= true;
+    }); 
+    $('#t').click(function(){
+      t= true;
+    }); 
+    $('#u').click(function(){
+      u= true;
+    }); 
+    $('#v').click(function(){
+      v= true;
+    }); 
+    $('#w').click(function(){
+      w= true;
+    }); 
+    $('#x').click(function(){
+      x= true;
+    }); 
+    $('#y').click(function(){
+      y= true;
+    }); 
+    $('#z').click(function(){
+      z= true;
+    }); 
+     $('#emoji1').click(function(){
+      emoji1= true;
+    }); 
+     $('#emoji2').click(function(){
+      emoji2= true;
+    }); 
+     $('#emoji3').click(function(){
+      emoji3= true;
+    }); 
+     $('#emoji4').click(function(){
+      emoji4= true;
+    }); 
+     $('#emoji5').click(function(){
+      emoji5= true;
+    }); 
+     $('#emoji6').click(function(){
+      emoji6= true;
+    }); 
+     $('#emoji7').click(function(){
+      emoji7= true;
+    }); 
+     $('#emoj8').click(function(){
+      emoji8= true;
+    }); 
+     $('#emoji9').click(function(){
+      emoji9= true;
+    }); 
+     $('#emoji10').click(function(){
+      emoji10= true;
+    }); 
+     $('#emoji11').click(function(){
+      emoji11= true;
+    }); 
+     $('#emoji12').click(function(){
+      emoji12= true;
+    }); 
+     $('#emoji13').click(function(){
+      emoji13= true;
+    }); 
+     $('#emoji14').click(function(){
+      emoji14= true;
+    }); 
+   
+    // move the item when you're mouse is down
+    $('body').mousemove(function(e){
+        // update mouse x and y position
+        xPos = e.pageX;
+        yPos = e.pageY;
+       
+        if(A== true){
+          $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #7FDBFF; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">A</div>');    
+        }
+      
+      if(B==true){
+         $('body').append('<div style="position: absolute;   font-family: Arimo; color: #0074D9; font-size: 200px; top:'+yPos+'px; left:'+xPos+'px">B</div>');
+      }
+      if(C==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#001f3f; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">C</div>');
+      }
+       if(D==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo; color: #39CCCC;  font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">D</div>');
+      }
+       if(E==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #3D9970; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">E</div>');
+      }
+        if(F== true){
+          $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #2ECC40; font-size: 110px; color: red; top:'+yPos+'px; left:'+xPos+'px">F</div>');
+            
+        }
+      if(G== true){
+          $('body').append('<div style="position: absolute;  font-family: Arimo;  font-size: 140px; color: #01FF70; top:'+yPos+'px; left:'+xPos+'px">G</div>');
+            
+        }
+      
+      if(H==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #FFDC00; font-size: 180px; top:'+yPos+'px; left:'+xPos+'px">H</div>');
+      }
+      if(I==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #FF851B; font-size: 170px; top:'+yPos+'px; left:'+xPos+'px">I</div>');
+      }
+       if(J==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#FF4136; font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">J</div>');
+      }
+       if(K==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #85144b; font-size: 120px; top:'+yPos+'px; left:'+xPos+'px">K</div>');
+      }
+      
+     if(L==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #F012BE; font-size: 190px; top:'+yPos+'px; left:'+xPos+'px">L</div>');
+      }       
+     if(M==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #B10DC9; font-size: 150px; top:'+yPos+'px; left:'+xPos+'px">M</div>');
+      }
+      if(N==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #BE9EC9; font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">N</div>');
+      }
+      if(O==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #006E6D; font-size: 90px; top:'+yPos+'px; left:'+xPos+'px">O</div>');
+      }
+      if(P==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #485167; font-size: 120px; top:'+yPos+'px; left:'+xPos+'px">P</div>');
+      }
+      if(Q==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #6F9FD8; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">Q</div>');
+      }
+      if(R==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #EC9787; font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">R</div>');
+      }
+      if(S==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #00A591; font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">S</div>');
+      }
+       if(T==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #4F84C4; font-size: 160px; top:'+yPos+'px; left:'+xPos+'px">T</div>');
+      }
+       if(U==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#F6D155; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">U</div>');
+      }
+       if(V==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #95DEE3; font-size: 170px; top:'+yPos+'px; left:'+xPos+'px">V</div>');
+      }
+       if(W==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#F7786B; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">W</div>');
+      }
+     if(X==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #034F84; font-size: 180px; top:'+yPos+'px; left:'+xPos+'px">X</div>');
+      }
+       if(Y==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: green; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">Y</div>');
+      }
+       if(Z==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #F7CAC9; font-size: 160px; top:'+yPos+'px; left:'+xPos+'px">Z</div>');
+      }
+      if(a==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #D65076; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">a</div>');
+      }
+      if(b==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #5B5EA6;font-size: 170px; top:'+yPos+'px; left:'+xPos+'px">b</div>');
+      }
+      if(c==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#BC243C; font-size: 120px; top:'+yPos+'px; left:'+xPos+'px">c</div>');
+      }
+      if(d==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#98B4D4; font-size: 160px; top:'+yPos+'px; left:'+xPos+'px">d</div>');
+      }
+      if(e==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#00FF00; font-size: 150px; top:'+yPos+'px; left:'+xPos+'px">e</div>');
+      }
+      if(f==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#BC8F8F; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">f</div>');
+      }
+      if(g==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#F0FFF0; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">g</div>');
+      }
+      if(h==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #2E8B57; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">h</div>');
+      }
+      if(i==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#9400D3; font-size: 90px; top:'+yPos+'px; left:'+xPos+'px">i</div>');
+      }
+      if(j==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#F5F5DC; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">j</div>');
+      }
+      if(k==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#BC8F8F; font-size: 110px; top:'+yPos+'px; left:'+xPos+'px">k</div>');
+      }
+      if(l==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #C71585; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">l</div>');
+      }
+      if(m==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #191970; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">m</div>');
+      }
+      if(n==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#FFDEAD; font-size: 110px; top:'+yPos+'px; left:'+xPos+'px">n</div>');
+      }
+      if(o==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #FFD700; font-size: 150px; top:'+yPos+'px; left:'+xPos+'px">o</div>');
+      }
+      if(p==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#F4A460; font-size: 130px; top:'+yPos+'px; left:'+xPos+'px">p</div>');
+      }
+      if(q==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#98FB98; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">q</div>');
+      }
+      if(r==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#DDA0DD; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">r</div>');
+      }
+      if(s==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#FF7F50; font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">s</div>');
+      }
+      if(t==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#87CEEB; font-size: 160px; top:'+yPos+'px; left:'+xPos+'px">t</div>');
+      }
+      if(u==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #4682B4; font-size: 190px; top:'+yPos+'px; left:'+xPos+'px">u</div>');
+      }
+      if(v==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#E0FFFF; font-size: 120px; top:'+yPos+'px; left:'+xPos+'px">v</div>');
+      }
+      if(w==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#9932CC; font-size: 180px; top:'+yPos+'px; left:'+xPos+'px">w</div>');
+      }
+      if(x==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #98FB98; font-size: 160px; top:'+yPos+'px; left:'+xPos+'px">x</div>');
+      }
+      if(y==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color:#FFE4E1; font-size: 110px; top:'+yPos+'px; left:'+xPos+'px">y</div>');
+      }
+      if(z==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  color: #6495ED; font-size: 140px; top:'+yPos+'px; left:'+xPos+'px">z</div>');
+      }
+        if(emoji1==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😂</div>');
+      }
+        if(emoji2==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 150px; top:'+yPos+'px; left:'+xPos+'px">😝</div>');
+      }
+        if(emoji3==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo; font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😁</div>');
+      }
+        if(emoji4==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  font-size: 150px; top:'+yPos+'px; left:'+xPos+'px">😱</div>');
+      }
+        if(emoji5==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😍</div>');
+      }
+        if(emoji6==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😉</div>');
+      }
+      if(emoji7==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😓</div>');
+      }
+        if(emoji8==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 100px; top:'+yPos+'px; left:'+xPos+'px">😳</div>');
+      }
+        if(emoji9==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😘</div>');
+      }
+        if(emoji10==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😜</div>');
+      }
+        if(emoji11==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">😡</div>');
+      }
+        if(emoji12==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;  font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">👿</div>');
+      }
+       if(emoji13==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo; font-size: 120px; top:'+yPos+'px; left:'+xPos+'px">💖</div>');
+      }
+        if(emoji14==true){
+         $('body').append('<div style="position: absolute;  font-family: Arimo;   font-size: 80px; top:'+yPos+'px; left:'+xPos+'px">💋</div>');
+      }
+      
+      
+      
+      
+     
+      });
+ 
+ });
+    
